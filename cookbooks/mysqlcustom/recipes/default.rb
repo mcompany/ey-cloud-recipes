06709c0b67fec496c3c2fec718eb70c20e441c14
mcom <mcom@mcompany.pl>
2015-09-04 10:43:08 +0200

    mysqlcustom recipe

--- cookbooks/mysqlcustom/recipes/default.rb
+++ cookbooks/mysqlcustom/recipes/default.rb
@@ -3,6 +3,11 @@
 # Recipe:: default
 #
 if node[:instance_role] == "db_master"
+	service 'mysql' do
+		supports :status => true
+		action [:enable, :start]
+	end
+
 	template '/etc/mysql/my.cnf' do
         	source 'my.cnf.erb'
 		owner "root"
